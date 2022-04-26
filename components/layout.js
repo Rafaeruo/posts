7b6feb0d2ea8e1667a174963011e3a3b0b44fffe
Rafael Scholz Griebler <rafaelscholz03@gmail.com>
2022-04-26 19:36:03 -0300

    paving the way

--- components/layout.js
+++ components/layout.js
@@ -1,20 +1,31 @@
-import Link from 'next/link';
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Layout({ children }) {
   return (
-    <div className='flex flex-col min-h-screen'>
-      <header className='bg-fuchsia-100 mb-8 py-4'>
-        <div className='container mx-auto flex justify-center'>
-          <Link href='/'>
+    <div className="flex flex-col min-h-screen">
+      <header className="px-4 bg-blue-100 mb-8 py-4">
+        <div className="container mx-auto flex justify-center">
+          <Link href="/">
             <a>🏡</a>
           </Link>
-          <span className='mx-auto'>Welcome to my blog</span>{' '}
+          <Link href="/">
+            <a className="mx-auto">Posts</a>
+          </Link>
         </div>
       </header>
-      <main className='container mx-auto flex-1'>{children}</main>
-      <footer className='bg-fuchsia-100 mt-8 py-4'>
-        <div className='container mx-auto flex justify-center'>
-          &copy; 2022 DailyDevTips
+      <main className="container mx-auto flex-1">{children}</main>
+      <footer className="mt-8 py-4">
+        <div className="container mx-auto flex justify-center">
+          <Link href="https://github.com/Rafaeruo/posts">
+            <a>
+              <Image
+                width={30}
+                height={30}
+                src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+              ></Image>
+            </a>
+          </Link>
         </div>
       </footer>
     </div>
